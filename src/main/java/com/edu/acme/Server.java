0e0e0b3e12f3077896b4399df8744a3db3b15041
acno <nosenerg@gmail.com>
2017-10-07 18:22:10 +0300

    Added Server

--- src/main/java/com/edu/acme/Server.java
+++ src/main/java/com/edu/acme/Server.java
@@ -1,5 +1,59 @@
 package com.edu.acme;
 
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Server {
-    
+    private static LinkedList<Socket> clientList = new LinkedList<>();
+    private static Queue<Message> messageQueue = new LinkedList<>();
+    private static final int PORT = 9999;
+
+    public static void main(String[] args) {
+        try (ServerSocket server = new ServerSocket(PORT)
+        ) {
+            new Thread(Server::sendToClientList);
+            while (true) {
+                Socket client = server.accept();
+                clientList.add(client);
+                new Thread(() -> clientProcess(client)).start();
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void clientProcess(Socket client) {
+        try {
+            BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+            String message;
+            do {
+                message = in.readLine();
+                messageQueue.add(new Message(message));
+                System.out.println(message);
+
+            } while (message != null);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void sendToClientList() {
+        while (true) {
+            if (messageQueue.size() > 0) {
+                for (int i = 0; i < clientList.size(); i++) {
+                    try (ObjectOutputStream out = new ObjectOutputStream(clientList.get(i).getOutputStream())) {
+                        out.writeObject(messageQueue.peek());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                messageQueue.poll();
+            }
+        }
+    }
 }
