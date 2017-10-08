80a64d554741a4abbb42d8020aeb586d5b6f25ed
ilyanovv <ilyanovv@gmail.com>
2017-10-08 17:49:01 +0300

    small refactoring

--- src/main/java/com/edu/acme/message/ServerMessage.java
+++ src/main/java/com/edu/acme/message/ServerMessage.java
@@ -3,29 +3,19 @@ package com.edu.acme.message;
 import com.edu.acme.Command;
 import java.io.ObjectOutputStream;
 
-/**
- * Класс, описывающий функционные сообщения от сервера к клиенту
- */
+
 public class ServerMessage extends Message {
-    /**
-     * Конструктор - создание нового функционного сообщения
-     * @param text текст сообщения
-     */
+
     public ServerMessage(String text) {
         super(text);
     }
 
-    /**
-     * Геттер поля команды в сообщении
-     */
     @Override
     public Command getCommand() {
         return null;
     }
 
-    /** Обработка сообщения
-     * @param out - поток вывода от сервера к клиенту
-     */
+
     @Override
     public void process(ObjectOutputStream out) {
 
